5947194dbabd8ebcf1642b468ea079a20e97415a
Gabriel Chavez <gabriel.chavez.r@gmail.com>
2022-07-23 06:19:17 -0400

    prueba

--- Aeronave.WebApi/Controllers/AeronaveController.cs
+++ Aeronave.WebApi/Controllers/AeronaveController.cs
@@ -14,8 +14,7 @@ namespace Aeronave.WebApi.Controllers
     {
         private readonly IMediator _mediator;
 
-        public AeronaveController(IMediator mediator)
-        {
+        public AeronaveController(IMediator mediator){
 
             _mediator = mediator;
         }

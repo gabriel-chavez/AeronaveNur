cb9a6065ddeae14a8a9de537582ed4b0f07dbf3b
UNIVIDA\lchavez <lchavez@univida.bo>
2023-05-23 19:27:32 -0400

    prueba

--- Aeronave.Application/UseCases/Command/RegistrarAeronave/RegistrarAeronaveHandler.cs
+++ Aeronave.Application/UseCases/Command/RegistrarAeronave/RegistrarAeronaveHandler.cs
@@ -10,16 +10,13 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Aeronave.Application.UseCases.Command.RegistrarAeronave
-{
-    public class RegistrarAeronaveHandler : IRequestHandler<RegistrarAeronaveCommand, Guid>
-    {
+namespace Aeronave.Application.UseCases.Command.RegistrarAeronave {
+    public class RegistrarAeronaveHandler : IRequestHandler<RegistrarAeronaveCommand, Guid> {
         private readonly IAeronaveRepository _aeronaveRepository;
         private readonly ILogger<RegistrarAeronaveHandler> _logger;
         private readonly IAeronaveFactory _aeronaveFactory;
         private readonly IUnitOfWork _unitOfWork;
-        public RegistrarAeronaveHandler(IAeronaveRepository aeronaveRepository, ILogger<RegistrarAeronaveHandler> logger, IAeronaveFactory aeronaveFactory, IUnitOfWork unitOfWork)
-        {
+        public RegistrarAeronaveHandler(IAeronaveRepository aeronaveRepository, ILogger<RegistrarAeronaveHandler> logger, IAeronaveFactory aeronaveFactory, IUnitOfWork unitOfWork) {
             _aeronaveRepository = aeronaveRepository;
             _logger = logger;
             _aeronaveFactory = aeronaveFactory;
@@ -27,29 +24,23 @@ namespace Aeronave.Application.UseCases.Command.RegistrarAeronave
         }
 
 
-        public async Task<Guid> Handle(RegistrarAeronaveCommand request, CancellationToken cancellationToken)
-        {
-            try
-            {
+        public async Task<Guid> Handle(RegistrarAeronaveCommand request, CancellationToken cancellationToken) {
+            try {
                 Aeronave.Domain.Model.Aeronaves.Aeronave aeronave = _aeronaveFactory.Crear(request.IdModelo, request.IdAereopuerto, request.Estado, request.Matricula);
-                if (request.MantenimientoAeronave != null)
-                {
-                    foreach (var item in request.MantenimientoAeronave)
-                    {
+                if (request.MantenimientoAeronave != null) {
+                    foreach (var item in request.MantenimientoAeronave) {
                         aeronave.AgregarItem(item.FechaInicio, item.FechaFin, item.Observaciones);
                     }
                 }
-                else
-                {
+                else {
                     aeronave.AgregarItem(DateTime.Now, DateTime.Now, "No se registro un mantenimiento programado");
                 }
                 aeronave.ConsolidarRegistro();
                 await _aeronaveRepository.CreateAsync(aeronave);
                 await _unitOfWork.Commit();
-                
-                return aeronave.Id;}
-            catch (Exception ex)
-            {
+                return aeronave.Id;
+            }
+            catch (Exception ex) {
                 _logger.LogError(ex, "Error al crear la aeronave");
             }
             return Guid.Empty;

cfc42e79f973d6f4e9a05f16df1b3c1e539c2a4d
gabriel-chavez <gabriel.chavez.r@gmail.com>
2023-05-31 21:12:42 -0400

    Update update-aeronave.sh

--- update-aeronave.sh
+++ update-aeronave.sh
@@ -10,3 +10,21 @@ git reset --hard HEAD
 git merge origin/$RAMA_REPO
 
 docker-compose up --build -d
+
+
+
+# Obtiene la lista de archivos .sql en la carpeta 'scripts'
+scripts=$(find scripts -type f -name '*.sql')
+
+# Parámetros de conexión a SQL Server
+server='34.118.139.240'
+port='1434'
+database='BD_PRUEBAS'
+username='sa'
+password='Gabriel123+'
+
+# Itera sobre cada script y ejecútalo utilizando sqlcmd
+for script in $scripts; do
+  sqlcmd -S "$server,$port" -d "$database" -U "$username" -P "$password" -i "$script"
+done
+

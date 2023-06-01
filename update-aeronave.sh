06e4f988de97600291b6f950a6b216f578d8681f
gabriel-chavez <gabriel.chavez.r@gmail.com>
2023-05-31 21:37:23 -0400

    Update update-aeronave.sh

--- update-aeronave.sh
+++ update-aeronave.sh
@@ -9,12 +9,13 @@ git fetch
 git reset --hard HEAD
 git merge origin/$RAMA_REPO
 
-docker-compose up --build -d
+# docker-compose up --build -d
 
 
 
-# Obtiene la lista de archivos .sql en la carpeta 'scripts'
-scripts=$(find scripts -type f -name '*.sql')
+# Ejecucion de scripts
+# Carpeta que contiene los scripts SQL
+carpeta_scripts="scripts"
 
 # Parámetros de conexión a SQL Server
 server='34.118.139.240'
@@ -23,9 +24,13 @@ database='BD_PRUEBAS'
 username='sa'
 password='Gabriel123+'
 
-# Itera sobre cada script y ejecútalo utilizando sqlcmd
-find "$carpeta_scripts" -type f -name "*.sql" -print0 | while IFS= read -r -d $'\0' archivo; do
-  echo "Ejecutando script: $archivo"
-  sqlcmd -S "$server,$port" -d "$database" -U "$username" -P "$password" -i "$archivo"
-done
+# Obtener la lista de archivos .sql en la carpeta y ordenarlos alfabéticamente
+archivos=$(ls -1 "$carpeta_scripts"/*.sql | sort)
 
+# Iterar sobre cada archivo .sql en orden alfabético y ejecutarlo
+for archivo in $archivos; do
+  if [ -f "$archivo" ]; then
+    echo "Ejecutando script: $archivo"
+    sqlcmd -S "$server,$port" -d "$database" -U "$username" -P "$password" -i "$archivo"
+  fi
+done
